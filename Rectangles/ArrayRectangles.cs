ac04ce2ccc6b826c96e664f8104ce1373d59f52a
Zholob, Nataliia <OSWNataliiaZ@oriflame.com>
2021-09-29 20:34:51 +0300

    Add project files.

---
+++ Rectangles/ArrayRectangles.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rectangles
+{
+    class ArrayRectangles
+    {
+        private Rectangle[] rectangleArray;
+
+        public ArrayRectangles(int length)
+        {
+            rectangleArray = new Rectangle[length];
+        }
+
+        public ArrayRectangles(Rectangle[] rectangles)
+        {
+            rectangleArray = rectangles;
+        }
+
+        public bool AddRectangle(Rectangle rectangle)
+        {
+            bool result = false;
+            for (int i = 0; i < rectangleArray.Length; i++)
+            {
+                if (rectangleArray[i] == null)
+                {
+                    rectangleArray[i] = rectangle;
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public int NumberMaxArea()
+        {
+            int result = -1;
+
+            return result;
+        }
+
+    }
+}
---
+++ Rectangles/Rectangle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rectangles
+{
+    class Rectangle
+    {
+        private int _sideA, _sideB;
+
+        public int sideA
+        {
+            get { return _sideA; }
+            set { _sideA = value; }
+        }
+        public int sideB
+        {
+            get { return _sideB; }
+            set { _sideB = value; }
+        }
+
+        public Rectangle(int a, int b)
+        {
+            sideA = a < 1 ? 0 : a;
+            sideB = b < 1 ? 0 : b;
+        }
+
+        public Rectangle(int a)
+        {
+            sideA = a < 1 ? 0 : a;
+            sideB = 5;
+        }
+        public Rectangle()
+        {
+            sideA = 4;
+            sideB = 3;
+        }
+
+        int GetSideA()
+        {
+            return sideA;
+        }
+
+        int GetSideB()
+        {
+            return sideB;
+        }
+
+        double Area()
+        {
+            return sideA * sideB;
+        }
+
+        double Perimeter()
+        {
+            return sideA + sideB;
+        }
+
+        bool IsSquare()
+        {
+            return sideA == sideB;
+        }
+
+        void ReplaceSides()
+        {
+            int temp = sideA;
+            sideA = sideB;
+            sideB = temp;
+        }
+    }
+}
